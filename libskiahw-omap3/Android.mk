13ebe28743a4f693f7b9035dd5d978aa60e88ca7
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-08-10 14:38:57 +0200

    fix libskiahw-omap3 Android.mk

--- libskiahw-omap3/Android.mk
+++ libskiahw-omap3/Android.mk
@@ -2,6 +2,21 @@ ifeq ($(TARGET_BOARD_PLATFORM),omap3)
 
 ifdef HARDWARE_OMX
 
+TOP := $(ANDROID_BUILD_TOP)
+
+TI_OMX_IMAGE ?= $(TOP)/hardware/ti/omx/image/src/openmax_il
+TI_OMX_COMP_C_INCLUDES ?= \
+	$(TOP)/hardware/ti/omx/system/src/openmax_il/lcml/inc \
+	$(TOP)/hardware/ti/omx/system/src/openmax_il/common/inc \
+	$(TOP)/hardware/ti/omx/system/src/openmax_il/omx_core/inc \
+	$(TOP)/frameworks/base/include/media/stagefright/openmax \
+	$(TOP)/hardware/ti/omap3/dspbridge/libbridge/inc 
+OMX_VENDOR_INCLUDES ?= $(TI_OMX_COMP_C_INCLUDES)
+
+TI_OMX_COMP_SHARED_LIBRARIES ?= libc libdl liblog libOMX_Core
+BOARD_OPENCORE_LIBRARIES ?= $(TI_OMX_COMP_SHARED_LIBRARIES)
+
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -10,6 +25,8 @@ LOCAL_ARM_MODE := arm
 
 # do not prelink
 LOCAL_PRELINK_MODULE := false
+
+LOCAL_REQUIRED_MODULES := libskia
 
 LOCAL_SHARED_LIBRARIES := \
 	libskia \
